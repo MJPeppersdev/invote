491599f2d28e223256991288f9dd5ff3e6b3b0cb
Michael J Peppers <mcgdevfunk824@protonmail.com>
2018-07-17 23:45:42 -0700

    Create core.js

---
+++ voting-server/src/core.js
@@ -0,0 +1,65 @@
+import {List, Map} from 'immutable';
+
+export const INITIAL_STATE = Map();
+
+export function setEntries(state, entries) {
+    const list = List(entries);
+    return state.setEntries('entries', list)
+                .set('initialEntries', list);
+}
+
+function getWinners(vote) {
+    if (!vote) return [];
+    const [one, two] = vote.get('pair');
+    const oneVotes = vote.getIn(['tally', one], 0);
+    const twoVotes = vote.getIn(['tally', two], 0);
+    if (oneVotes > twoVotes) return [one];
+    else if (oneVotes < twoVotes) return[two];
+    else                          return [one, two];
+}
+
+export function next(state, round = state.getIn(['vote', 'round'], 0)) {
+    const entries = state.get('entries')
+                         .concat(getWinners(state.get('vote')));
+    if (entries.size === 1) {
+        return state.remove('vote')
+                    .remove('entries')
+                    .set('winner', entries.first());
+    } else {
+        return state.merge({
+            vote: Map({
+                round: round + 1,
+                pair: entries.take(2) 
+            }),
+            entries: entries.skip(2)
+        });
+    }
+}
+
+export function restart(state) {
+    const round = state.getIn(['vote', 'round'], 0);
+    return next(
+        state.set('entries', state.get('initialEntries'))
+             .remove('vote')
+             .remove('winner'),
+        round
+    );
+}
+
+function removePreviousVote(voteState, voter) {
+    const previousVote = vote.State.getIn(['votes', voter]);
+    if (previousVote) {
+        return voteState.updateIn(['tally', previousVote], t=> t - 1)
+                        .removeIn(['votes', voter]);
+    } else {
+        return voteState;
+    }
+}
+
+export function vote(voteState, entry, voter) {
+    return addVote(
+        removePreviousVote(voteState, voter),
+        entry,
+        voter
+    );
+}
